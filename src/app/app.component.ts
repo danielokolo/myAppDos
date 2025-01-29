dac6476019890ad8742a99b89efab63a36d308d1
danielokolo <dokolosanchez611@gmail.com>
2025-01-29 13:39:11 +0100

    routingFundionanding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-
+import {Component} from '@angular/core';
+import {HomeComponent} from './home/home.component';
+import {RouterModule} from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
-`,
-  styles: [],
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  styleUrls: [],
 })
 export class AppComponent {
-  title = 'myAppDos';
+  title = 'homes';
 }
